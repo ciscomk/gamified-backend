374c013a37a836d25eadbd198c2b9c4bdae4a82f
ciscomk <mike@cisco.com>
2017-05-08 22:23:50 +0000

    added scripts to switch lab headings

---
+++ app/templates/switch2bgp.sh
@@ -0,0 +1,2 @@
+find . -type f -name "*.html" -exec sed -i 's/LTRRST-2016/LTRRST-1179/g' {} +
+find . -type f -name "*.html" -exec sed -i 's/IPv6 For Fun and Fake Profit/BGP For Fun and Fake Profit/g' {} +
---
+++ app/templates/switch2ipv6.sh
@@ -0,0 +1,2 @@
+find . -type f -name "*.html" -exec sed -i 's/LTRRST-1179/LTRRST-2016/g' {} +
+find . -type f -name "*.html" -exec sed -i 's/BGP For Fun and Fake Profit/IPv6 For Fun and Fake Profit/g' {} +
